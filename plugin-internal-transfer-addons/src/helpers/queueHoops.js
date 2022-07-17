e63975aa865e531d5d0ec79ae67fcfdf3973b99b
trogers-twilio <46937354+trogers-twilio@users.noreply.github.com>
2022-07-16 17:02:18 -0700

    Enhanced closed text in directory queue item

--- plugin-internal-transfer-addons/src/helpers/queueHoops.js
+++ plugin-internal-transfer-addons/src/helpers/queueHoops.js
@@ -33,124 +33,18 @@ export const loadHoops = (storeHoopsAction) => {
   });
 };
 
-// Evaluates Hours Of Operation data and compares to "now"
-// to create a Queue filter that shows only available Queues
-export const evalHoops = () => {
-
-  // The HOOP data
-  const theData = manager.store.getState()['internal-transfer-addons'].hoopData.hoopData;
-
-  // Need to find queue names that are open now!
-  // Where "now" is translated to the timezone
-  // used in the Hours Of Operation json
-
-  const formatOptions = {
-    timeZone: theData.timezone,
-    weekday: 'short',
-    hour: 'numeric',
-    hour12: false,
-    month: 'short',
-    day: 'numeric'
-  };
-  const formatter = new Intl.DateTimeFormat('en-US', formatOptions);
-
-  // Get the current time, day of the week, and month/day for agent's time zone
-  const formattedDate = formatter.format(new Date()).split(', ');
-
-  const hour = formattedDate[2];
-  const day = formattedDate[0];
-  const monthDay = formattedDate[1];
-
-  const invalidKeys = ['timezone', 'holidays'];
-
-  // Convert to query syntax
-  let newFilters = [];
-  let subAry = [];
-  let i = 1;
-
-  for( const [key, value] of Object.entries(theData)) {
-    // This means value is object listing days and open/close
-    if(!(invalidKeys.includes(key)) 
-        && value[day].open <= hour 
-        && value[day].close > hour
-        && !(value.holidays.includes(monthDay))) {
-      subAry.push('"' + key + '"');
-    }
-    if(subAry.length == 29) {
-      newFilters.push(subAry.join(", "));
-      subAry = [];
-    }
-  }
-
-  if(subAry.length > 0) {
-    newFilters.push(subAry.join(", "));
-  }
-
-  let newQuery = 'data.queue_name IN [' + newFilters.join('] OR data.queue_name IN [') + ']';
-
-  return newQuery;
-};
-
-export const getClosedQueues = () => {
-  // The HOOP data
-  const theData = manager.store.getState()['internal-transfer-addons'].hoopData.hoopData;
-
-  const formatOptions = {
-    timeZone: theData.timezone,
-    weekday: 'short',
-    hour: 'numeric',
-    hour12: false,
-    month: 'short',
-    day: 'numeric'
-  };
-  const formatter = new Intl.DateTimeFormat('en-US', formatOptions);
-
-  // Get the current time, day of the week, and month/day for agent's time zone
-  const formattedDate = formatter.format(new Date()).split(', ');
-
-  const hour = formattedDate[2];
-  const day = formattedDate[0];
-  const monthDay = formattedDate[1];
-
-  const invalidKeys = ['timezone', 'holidays'];
-
-  const closedQueues = new Map();
-
-  for( const [key, value] of Object.entries(theData)) {
-    if (invalidKeys.includes(key)) {
-      // No need to evaluate open/close hours since this is not a queue
-      continue;
-    }
-    
-    // This means value is object listing days and open/close
-    const queueName = key;
-    const queueSid = value.sid;
-    const queueOpenHour = value[day].open;
-    const queueCloseHour = value[day].close;
-    const queueHolidays = value.holidays;
-
-    const isTodayHoliday = queueHolidays.includes(monthDay);
-
-    const isQueueClosed = isTodayHoliday
-      || hour < queueOpenHour
-      || hour >= queueCloseHour;
-
-    if(isQueueClosed) {
-      console.debug('getClosedQueues, adding queue to closedQueues:', queueSid, queueName);
-      closedQueues.set(queueSid, {
-        queueName,
-        queueOpenHour,
-        queueCloseHour,
-        isTodayHoliday
-      });
-    }
-  }
-
-  return closedQueues;
-};
-
 export const getQueueHoopForToday = (queueSid) => {
-  const hoops = manager.store.getState()['internal-transfer-addons'].hoopData.hoopData;
+  const hoops = manager.store.getState()['internal-transfer-addons']?.hoopData?.hoopData;
+
+  if (!hoops) {
+    return;
+  }
+
+  const queueHoop = hoops[queueSid];
+
+  if (!queueHoop) {
+    return;
+  }
 
   const dateFormatOptions = {
     timeZone: hoops.timezone,
@@ -165,7 +59,6 @@ export const getQueueHoopForToday = (queueSid) => {
 
   // Get the current date formatted for defined timezone and split into parts
   const formattedDateParts = dateFormatter.formatToParts(new Date());
-  console.debug('getQueueHoopForToday, formattedDateParts24H:', formattedDateParts);
 
   const weekday = formattedDateParts.find(d => d.type === 'weekday')?.value;;
   const month = formattedDateParts.find(d => d.type === 'month')?.value;
@@ -174,23 +67,11 @@ export const getQueueHoopForToday = (queueSid) => {
   const hour = formattedDateParts.find(d => d.type === 'hour')?.value;
   const timezoneName = formattedDateParts.find(d => d.type === 'timeZoneName')?.value;
 
-  const queueHoop = hoops[queueSid];
-
-  console.debug('getQueueHoopForToday, queueHoop:', queueHoop);
-
-
-  if (!queueHoop) {
-    // Unable to find matching queue in the HOOP data, so defaulting to not closed
-    return false;
-  }
-
   const queueName = queueHoop.friendlyName;
+  const matchedHoop = queueHoop.matchedHoop;
   const queueOpenHour = queueHoop[weekday].open;
   const queueCloseHour = queueHoop[weekday].close;
   const queueHolidays = queueHoop.holidays;
-
-  console.debug('getQueueHoopForToday, queueOpenHour:', queueOpenHour);
-  console.debug('getQueueHoopForToday, queueCloseHour:', queueCloseHour);
 
   const isTodayHoliday = Array.isArray(queueHolidays) && queueHolidays.includes(monthDay);
 
@@ -198,15 +79,29 @@ export const getQueueHoopForToday = (queueSid) => {
     || hour < queueOpenHour
     || hour >= queueCloseHour
 
+  const formatHour = (hourToFormat) => {
+    return hourToFormat === 0
+      ? 12
+      : hourToFormat > 12
+        ? hourToFormat - 12
+        : hourToFormat;
+  };
+
   const openHourDayPeriod = queueOpenHour < 12 ? 'AM' : 'PM';
   const closeHourDayPeriod = queueCloseHour < 12 ? 'AM' : 'PM';
-  const formattedOpenHour = `${queueOpenHour <= 12 ? queueOpenHour : queueOpenHour - 12}:00 ${openHourDayPeriod}`;
-  const formattedCloseHour = `${queueCloseHour <= 12 ? queueCloseHour : queueCloseHour - 12}:00 ${closeHourDayPeriod}`;
+  const formattedOpenHour = `${formatHour(queueOpenHour)}:00 ${openHourDayPeriod}`;
+  const formattedCloseHour = `${formatHour(queueCloseHour)}:00 ${closeHourDayPeriod}`;
+  const shortOpenHour = `${formatHour(queueOpenHour)} ${openHourDayPeriod}`;
+  const shortCloseHour = `${formatHour(queueCloseHour)} ${closeHourDayPeriod}`;
 
   return {
+    matchedHoop,
     queueName,
+    queueSid,
     queueOpenHour: formattedOpenHour,
     queueCloseHour: formattedCloseHour,
+    shortOpenHour,
+    shortCloseHour,
     timezoneName,
     isQueueClosed,
     isTodayHoliday
--- plugin-internal-transfer-addons/src/strings/index.js
+++ plugin-internal-transfer-addons/src/strings/index.js
@@ -5,29 +5,58 @@ import { queueHoops }  from '../helpers';
 const manager = Manager.getInstance();
 
 const registerHandlebarsHelpers = () => {
-  window.Handlebars.registerHelper('CustomWorkerDirectoryQueueItemText', (payload) => {
-    console.debug('CustomWorkerDirectoryQueueItemText, payload:', payload);
-    const queue = payload?.data?.root?.queue;
+  window.Handlebars.registerHelper('WorkerDirectoryIsQueueClosed', function (payload, options) {
+    const queueSid = payload?.sid
+    const fnTrue = options?.fn;
+    const fnFalse = options?.inverse;
 
-    if (!queue) {
-      return '';
+    if (!queueSid) {
+      return fnFalse(this);
     }
 
-    const queueHoop = queueHoops.getQueueHoopForToday(queue.sid);
+    const queueHoop = queueHoops.getQueueHoopForToday(queueSid);
 
     if (!queueHoop) {
-      return queue.name;
+      return fnFalse(this);
     }
 
-    return queueHoop.isQueueClosed
-      ? `CLOSED: ${queue.name}`
-      : queue.name;
+    const response = {
+      ...this,
+      queueOpenHour: queueHoop.queueOpenHour,
+      queueCloseHour: queueHoop.queueCloseHour,
+      shortOpenHour: queueHoop.shortOpenHour,
+      shortCloseHour: queueHoop.shortCloseHour,
+      timezoneName: queueHoop.timezoneName,
+      isQueueClosed: queueHoop.isQueueClosed,
+      isTodayHoliday: queueHoop.isTodayHoliday
+    }
+
+    return queueHoop.isQueueClosed ? fnTrue(response) : fnFalse(response);
   });
 };
 
 export const initializeStrings = () => {
   registerHandlebarsHelpers();
 
-  manager.strings.WorkerDirectoryQueueItemText = '{{CustomWorkerDirectoryQueueItemText}}';
+  const isHoopsEnabled = manager.serviceConfiguration.ui_attributes?.internalTransferAddonsPlugin?.isHoopsEnabled;
+
+  if (isHoopsEnabled) {
+    const closedStyle = 'color:maroon';
+
+    manager.strings.WorkerDirectoryQueueItemText = `
+      {{#WorkerDirectoryIsQueueClosed queue}}
+        <div>{{queue.name}}</div>
+        <div>
+          <span style=${closedStyle}>CLOSED </span>
+          {{#if isTodayHoliday}}
+            <span style=${closedStyle}>Holiday </span>
+          {{/if}}
+          <span>(Hours: {{shortOpenHour}} - {{shortCloseHour}} {{timezoneName}})</span>
+        </div>
+      {{else}}
+        {{queue.name}}
+      {{/WorkerDirectoryIsQueueClosed}}
+    `;
+  }
 };
 
